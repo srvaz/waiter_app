42aa64781bc972d481ada027716ed37c18fe2677
srvaz <jonwify@gmail.com>
2020-05-09 14:33:54 -0300

    :white_check_mark: Update test

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:waiter_app/pages/listPage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(WaiterApp());
 }
 
-class MyApp extends StatelessWidget {
+class WaiterApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ListPage()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Waiter App',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ListPage());
   }
 }
